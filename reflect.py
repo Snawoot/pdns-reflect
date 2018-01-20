5c43999af3cfe8fab63d7d041945b76af022de53
Vladislav Yarmak <vladislav@vm-0.com>
2018-01-20 18:55:08 +0000

    first results

--- reflect.py
+++ reflect.py
@@ -14,17 +14,20 @@ def detect_af(addr):
 
 
 def write_response(resp):
-    for record in response:
+    for record in resp:
         sys.stdout.write('\t'.join(record) + '\n')
     sys.stdout.flush()
 
 
 class QueryHandler(object):
     def __init__(self):
-        pass
+        self.qtable = {
+            'HELO': self.handle_helo,
+            'Q': self.handle_q,
+        }
 
 
-    def handle_badcmd(self, _):
+    def make_fail(self, _):
         return [['FAIL']]
 
 
@@ -32,11 +35,21 @@ class QueryHandler(object):
         try:
             ver = int(q[1])
             if ver == 1:
-                return ['OK', 'reflect.py is ready']
+                return [['OK', 'reflect.py is ready']]
             else:
-                return handle_badcmd(q)
-        except
-            return handle_badcmd(q)
+                return self.make_fail(q)
+        except:
+            return self.make_fail(q)
+
+
+    def make_a_resp(self, q):
+        return [['DATA', q[1], 'IN', 'A', '0', '1', q[5]],
+                ['END']]
+
+
+    def make_aaaa_resp(self, q):
+        return [['DATA', q[1], 'IN', 'AAAA', '0', '1', q[5]],
+                ['END']]
 
 
     def handle_q(self, q):
@@ -45,20 +58,22 @@ class QueryHandler(object):
             if qclass != 'IN':
                 return [['LOG', 'Only IN addresses are supported'],
                         ['FAIL']]
-            if qtype in 
 
-        except:
-            return handle_badcmd(q)
+            af = detect_af(remote_ip)
+            if af == socket.AF_INET and qtype in ('A', 'ANY'):
+                return self.make_a_resp(q)
+            elif af == socket.AF_INET6 and qtype in ('AAAA', 'ANY'):
+                return self.make_aaaa_resp(q)
+            else:
+                return self.make_fail(q)
 
-
-    qtable = {
-        'HELO': handle_hello,
-        'Q': handle_q,
-    }
+        except Exception as e:
+            return [['LOG', 'Exception: %s' % str(e)],
+                    ['FAIL']]
 
 
     def handle(self, q):
-        return qtable.get(q[0], handle_badcmd)(self, q)
+        return self.qtable.get(q[0], self.make_fail)(q)
 
 
 def main():
@@ -71,7 +86,7 @@ def main():
             query = line.rstrip('\n').split('\t')
             resp = handler.handle(query)
             write_response(resp)
-    except KeyboadInterrupt:
+    except KeyboardInterrupt:
         sys.stdout.flush()
 
 
